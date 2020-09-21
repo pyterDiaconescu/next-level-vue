9c69414a582c91b872f62c2ce7114355a22ab6d8
petrero <petrero@gmail.com>
2020-09-21 18:48:36 +0300

    5.2. Completing our progress bar
    Now we need to specify a prop because that's how we're going to receive the page we are on.
    Now we write our beforeRouteUpdate. BeforeRouteUpdate needs exact the same code as BeforeRouteEnter so we extract the code into a function.
    Everything works now.

--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -17,24 +17,37 @@ import EventCard from '@/components/EventCard.vue'
 import { mapState } from 'vuex'
 import store from '@/store/store';
 
+function getPageEvents(routeTo, next){
+  const currentPage = parseInt(routeTo.query.page) || 1
+  store
+    .dispatch('event/fetchEvents', {
+      page: currentPage
+    })
+    .then(() => {
+      routeTo.params.page = currentPage
+      next()
+    })
+}
+
 export default {
+  props: {
+    page: {
+      type: Number,
+      required: true
+    }
+  },
   components: {
     EventCard
   },
   beforeRouteEnter(routeTo, routeFrom, next){
-    const currentPage = parseInt(routeTo.query.page) || 1
-    store
-      .dispatch('event/fetchEvents', {
-        page: currentPage
-      })
-      .then(() => {
-        routeTo.params.page = currentPage
-        next()
-      })
+    getPageEvents(routeTo, next)
+  },
+  beforeRouteUpdate(routeTo, routeFrom, next){
+    getPageEvents(routeTo, next)
   },
   computed: {
     hasNextPage() {
-      return this.event.eventsTotal > this.page * this.perPage
+      return this.event.eventsTotal > this.page * this.event.perPage
     },
     ...mapState(['event', 'user'])
   }
