3595ef23168e9f2ee2b35ebd74aa4cef70600a97
petrero <petrero@gmail.com>
2020-09-22 06:40:45 +0300

    6.3. Error handling
    Create another component to display in case our api is stoped or network is slow. Our user will not wait forever to get the event

--- src/router.js
+++ src/router.js
@@ -4,6 +4,7 @@ import EventCreate from './views/EventCreate.vue'
 import EventList from './views/EventList.vue'
 import EventShow from './views/EventShow.vue'
 import NotFound from '@/views/NotFound'
+import NetworkIssue from '@/views/NetworkIssue'
 import NProgress from 'nprogress'
 import store from '@/store/store';
 
@@ -35,7 +36,13 @@ const router = new Router({
             routeTo.params.event = event
             next()
           })
-          .catch(() => next({ name: '404', params: { resource: 'event'}}))
+          .catch((error) => {
+            if (error.response && error.response.status === 404){
+              next({ name: '404', params: { resource: 'event'}})
+            } else {
+              next({ name: 'network-issue'})
+            }
+          })
       }
     },
     {
@@ -43,6 +50,11 @@ const router = new Router({
       name: '404',
       component: NotFound,
       props: true
+    },
+    {
+      path: '/network-issue',
+      name: 'network-issue',
+      component: NetworkIssue
     },
     {
       path: '*',
